8bed00570c5c3f6c31f308666eb5aaec53b7e86f
AthK <51260976+newaccforhc@users.noreply.github.com>
2020-09-09 14:07:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
-unzip -q rclone-v1.52.3-linux-amd64.zip
-export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
+wget -q https://downloads.rclone.org/v1.53.0/rclone-v1.53.0-linux-amd64.zip
+unzip -q rclone-v1.53.0-linux-amd64.zip
+export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
